1237ab27b36908e43c9ff17175ba188d0aa30bc9
X-jy-liu <jingyang__liu@outlook.com>
2025-01-24 16:14:15 +0000

    ver2: dealed with overlaps meth_1 and meth_2 to be continued

--- Chubb Limited.py
+++ Chubb Limited.py
@@ -11,7 +11,6 @@ ticker = "CB"  # Chubb Limited
 # # Download hourly data using yfinance
 # data = yf.download(ticker, start=start_date, end=end_date, interval="1h")
 # data.to_csv('Chubb_Limited_Hourly_Kline.csv')
-# The columns of the data are in order: close, high, low, open, volume
 
 file_path = 'Chubb_Limited_Hourly_Kline.csv'
 data = pd.read_csv(file_path)
@@ -20,10 +19,46 @@ data['Datetime'] = pd.to_datetime(data['Datetime'])
 
 # Set the 'Datetime' column as the index
 data.set_index('Datetime', inplace=True)
-print(data.head())
-
-
+# print(data.head(3))
+# print(f'1st Close Price',data.iloc[0,0])
+# print(f'1st High Price',data.iloc[0,1])
+# print(f'1st Low Price',data.iloc[0,2])
+# print(f'1st Open Price',data.iloc[0,3])
+# print(f'1st Volume',data.iloc[0,4])
 patterns = []
+closest_pattern = 'none'
+idx_to_drop = []
+# The columns of the data are in order: close, high, low, open, volume
+# 先判断顶底分型，然后导出处理了包含关系后的数据
+for i in range(1, len(data) - 1):
+    prev, curr, next_ = data.iloc[i - 1], data.iloc[i], data.iloc[i + 1]
+    # 判断顶分型
+    if curr[1] > prev[1] and curr[1] > next_[1] and \
+        curr[2] > prev[2] and curr[2] > next_[2]:
+        # patterns.append(('top', i))
+        closest_pattern = 'top'
+
+    # 判断底分型
+    if curr[1] < prev[1] and curr[1] < next_[1] and \
+        curr[2] < prev[2] and curr[2] < next_[2]:
+        # patterns.append(('bottom', i))
+        closest_pattern = 'bottom'
+
+    # 右包含检测，只考虑两个K线之间的包含关系
+    if prev[1] > curr[1] and prev[2] < curr[2]:
+        # 当右包含出现时，上一个分型为顶分型
+        if closest_pattern == 'top':
+            data.iloc[i]['low'] = prev[2]
+        # 当右包含出现时，上一个分型为底分型
+        if closest_pattern == 'bottom':
+            data.iloc[i]['high'] = prev[1]
+        # 把i-1行的数据删除
+        idx_to_drop.append(i-1)
+
+data = data.drop(data.index[idx_to_drop])
+
+
+# 将已经处理过包含关系的数据重新判断顶分底分型
 for i in range(1, len(data) - 1):
     prev, curr, next_ = data.iloc[i - 1], data.iloc[i], data.iloc[i + 1]
     # 判断顶分型
@@ -100,7 +135,7 @@ mpf.plot(
     data,
     type='candle',      # K线
     addplot=plots,
-    title=f"{ticker} Hourly Candlestick with Patterns",
+    title=f"{ticker} Hourly Candlestick with Patterns - remove i-1 if i-1's extreme values including i's",
     figsize=(12, 8)
 )
 
